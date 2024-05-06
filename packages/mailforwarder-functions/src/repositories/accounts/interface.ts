8689a5328bda54e879e40c1bbe175b4bb96d6611
Nobuhiro Ito <iseebi@iseteki.net>
2024-05-06 10:44:02 +0900

    implement drop

--- packages/mailforwarder-functions/src/repositories/accounts/interface.ts
+++ packages/mailforwarder-functions/src/repositories/accounts/interface.ts
@@ -2,4 +2,5 @@ import { Account } from "../../models";
 
 export interface AccountsRepository {
   getAsync(accountId: string): Promise<Account | undefined>;
+  isDropAsync(accountId: string, recipient: string): Promise<boolean | undefined>;
 }
--- packages/mailforwarder-functions/src/usecases/forwarding/implements.ts
+++ packages/mailforwarder-functions/src/usecases/forwarding/implements.ts
@@ -13,6 +13,7 @@ class ForwardingUseCaseImplementation implements ForwardingUseCase {
   }
 
   public async handleForwardEventAsync(inputForwarding: Forwarding): Promise<void> {
+    // Pre-check
     const forwarding = await this.forwardingRepository.getAsync(inputForwarding.forwardingId);
     if (!forwarding) {
       console.warn(`[${inputForwarding.forwardingId}] forwarding not found`);
@@ -22,12 +23,24 @@ class ForwardingUseCaseImplementation implements ForwardingUseCase {
       console.warn(`[${inputForwarding.forwardingId}] already finished`);
       return;
     }
+
+    // Account
     const account = await this.accountsRepository.getAsync(forwarding.accountId);
     if (!account) {
       console.warn(`[${inputForwarding.forwardingId}] account not found`);
       await this.forwardingRepository.markFailedAsync(forwarding.forwardingId);
       return;
     }
+
+    // Drop
+    const drop = await this.accountsRepository.isDropAsync(account.accountId, forwarding.recipient);
+    if (drop) {
+      console.info(`[${inputForwarding.forwardingId}] drop`);
+      await this.forwardingRepository.markDroppedAsync(forwarding.forwardingId);
+      return;
+    }
+
+    // Forwarding
     console.info(`[${inputForwarding.forwardingId}] forwarding start`);
     await this.forwardingRepository.executeForwardAsync(forwarding, account.accountEmail);
     console.info(`[${inputForwarding.forwardingId}] forwarding end`);
